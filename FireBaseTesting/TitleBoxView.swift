6c00aa6f90f85440915dd13328876a0dcbfc659a
srfnmnk <daniel.tomes@cloudera.com>
2016-12-26 12:06:07 -0500

    Wired Up Buttons to Google FireBase

---
+++ FireBaseTesting/TitleBoxView.swift
@@ -0,0 +1,33 @@
+//
+//  TitleBoxView.swift
+//  FireBaseTesting
+//
+//  Created by Daniel Tomes on 12/26/16.
+//  Copyright © 2016 Daniel Tomes. All rights reserved.
+//
+
+import UIKit
+
+class TitleBoxView: UIView {
+
+    
+
+    //override func draw(_ rect: CGRect) {
+        
+        
+        
+        /*
+        let rectWidth = min(
+            bounds.size.width / 4,
+            bounds.size.width - bounds.size.width / 12)
+        */
+        
+        
+        //UIBezierPath* rectanglePath = [UIBezierPath bezierPathWithRect: CGRectMake(68, 38, 81, 40)];
+        //[UIColor.grayColor setFill];
+        //[rectanglePath fill];
+        
+        // Drawing code
+    //}
+
+}
--- FireBaseTesting/ViewController.swift
+++ FireBaseTesting/ViewController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TitleBoxView: TitleBoxView!
+    @IBOutlet weak var DebugLabel: UILabel!
+    
+    @IBAction func ReleaseKraken(_ sender: UIButton) {
+        FIRAnalytics.logEvent(withName: "Release_Kraken", parameters: nil)
+    }
+    
+    @IBAction func SquidFormSwitch(_ sender: UISwitch) {
+        FIRAnalytics.logEvent(withName: "Squid_Form", parameters: nil)
+    }
+    
+    @IBAction func AggressionChanged(_ sender: UISlider) {
+        DebugLabel.text = String(sender.value)
+        FIRAnalytics.logEvent(withName: "Aggression_Change", parameters: ["newValue": sender.value as NSObject])
+    }
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
